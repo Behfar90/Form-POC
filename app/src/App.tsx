395e74a5a8cb44019ea1b5cd3b97d18255ae6d9c
BeHFaR <behfar.behzad@gmail.com>
2026-06-04 10:18:32 +0200

    feat(App): add ground foundation for form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,59 @@
-import "./App.css";
+import { useState } from "react";
+import type { FormDetails } from "./types/form";
+import styles from "./App.module.css";
+import mockData from "../mockData.json";
+import { Loader } from "lucide-react";
 
-function App() {
-  return <h1>Spond Form</h1>;
+export default function App() {
+  const [formConfig] = useState<FormDetails>(mockData as FormDetails);
+  const [currentStep, setCurrentStep] = useState(1);
+
+  if (!formConfig)
+    return (
+      <div>
+        Loading... <Loader />
+      </div>
+    );
+
+  if (new Date(formConfig.registrationOpens) > new Date()) {
+    return (
+      <div className={styles.card}>
+        <h2>Registration Not Open</h2>
+        <p>
+          Opens on {new Date(formConfig.registrationOpens).toLocaleDateString()}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.card}>
+      <h2>{formConfig.title}</h2>
+
+      <form
+        className={styles.form}
+        onSubmit={() => console.log("Submit form data")}
+      >
+        {currentStep === 1 && <div>Step 1</div>}
+
+        {currentStep === 2 && <div>Step 2</div>}
+
+        {currentStep === 3 && <div>Step 3</div>}
+
+        <div className={styles.buttonContainer}>
+          <button
+            type="button"
+            className={styles.button}
+            disabled={currentStep === 1}
+            onClick={() => setCurrentStep((prev) => prev - 1)}
+          >
+            Back
+          </button>
+          <button type="submit" className={styles.button}>
+            {currentStep === 3 ? "Submit" : "Next"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
-
-export default App;
