3b0943a50088007e2020c751e0f2c498641a6049
BeHFaR <behfar.behzad@gmail.com>
2026-06-04 17:23:31 +0200

    feat(main): add main function to root api

---
+++ api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	_ "api/db"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods: "GET, POST",
+	}))
+
+	// route handlers here
+
+	log.Fatal(app.Listen(":8080"))
+}
